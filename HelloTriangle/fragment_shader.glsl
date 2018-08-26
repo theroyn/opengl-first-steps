3282cb90800bced0f53798110716058d65b6bd65
theroyn <theroyn@gmail.com>
2018-08-26 19:09:53 +0300

    Improved fragment shader and multiple vbo's per vao

--- HelloTriangle/fragment_shader.glsl
+++ HelloTriangle/fragment_shader.glsl
@@ -1,9 +1,9 @@
 #version 400
 
-in vec3 frag_initial_colour;
+in vec3 colour;
 out vec4 frag_colour;
 
 void main()
 {
-  frag_colour = vec4(0.0, 1.0, 0.0, 1.0);
+  frag_colour = vec4(colour, 1.0);
 }
--- HelloTriangle/vertex_shader.glsl
+++ HelloTriangle/vertex_shader.glsl
@@ -1,8 +1,12 @@
 #version 400
 
-in vec3 vp;
+layout(location = 0) in vec3 vp;
+layout(location = 1) in vec3 vc;
+
+out vec3 colour;
 
 void main()
 {
   gl_Position = vec4(vp, 1.0);
+  colour = vc;
 }
