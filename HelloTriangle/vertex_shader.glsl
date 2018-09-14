654e65ac516af770bb3396a9253ff52bb88d76c8
theroyn <theroyn@gmail.com>
2018-09-14 12:04:15 +0300

    Pushing before creating something besides r_draw_triangle().

--- HelloTriangle/vertex_shader.glsl
+++ HelloTriangle/vertex_shader.glsl
@@ -1,12 +1,14 @@
 #version 400
 
-layout(location = 0) in vec3 vp;
-layout(location = 1) in vec3 vc;
+layout(location = 0) in vec3 pos;
+layout(location = 1) in vec3 col;
 
 out vec3 colour;
+uniform float light;
 
 void main()
 {
-  gl_Position = vec4(vp, 1.0);
-  colour = vc;
+  gl_Position = vec4(pos, 1.0);
+  //colour = col*light;
+  colour = pos;
 }
